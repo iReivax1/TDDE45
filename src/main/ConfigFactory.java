c573f22f7f7abfa9654b2ff83875a0591c2965fd
Xavier Tan <xavi0007@e.ntu.edu.sg>
2019-09-08 17:23:56 +0200

    doned

--- src/main/ConfigFactory.java
+++ src/main/ConfigFactory.java
@@ -7,18 +7,21 @@ import yaml.YamlBuildConfigurationReader;
 
 public class ConfigFactory {
 	Object configurationReader = null;
-	System.out.println("Building target init");
+
 	public static ConfigInterface getAdapter(String configOption) {
 		ConfigInterface ci = null;
 		
 		if(configOption.equals("X") || configOption.equals("x")) {
 			ci = new XMLAdapter();
+			System.out.println("Building target init");
 		} 
 		else if(configOption.equals("Y") || configOption.equals("y")) {
 			ci = new YAMLAdapter();
+			System.out.println("Building target init");
 		} else {
 			// XML by default
 			ci = new XMLAdapter();
+			System.out.println("Building target init");
 		}
 		return ci;
 	}
